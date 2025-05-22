13ff15e2dcb333658fb783a60815515800b3f6e5
charles-togle <149758871+charles-togle@users.noreply.github.com>
2025-05-22 23:17:34 +0800

    migrated to vite + tailwind and ts

---
+++ vite.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite'
+import { resolve, dirname } from 'path'
+import react from '@vitejs/plugin-react'
+import { fileURLToPath } from 'url'
+import tailwindcss from '@tailwindcss/vite'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src')
+    }
+  }
+})
