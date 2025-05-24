2189879b6bf708eebe7bf988e20b473d14395690
charles-togle <149758871+charles-togle@users.noreply.github.com>
2025-05-24 13:01:08 +0800

    images, and global css

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,21 @@
-import './App.css'
-import AddEvents from './sections/AddEvents'
-import ArtOrganizations from './sections/ArtOrganization'
-import BrandFeatured from './sections/BrandFeature'
-import CityGuides from './sections/CityGuides'
-import Footer from './sections/Footer'
-import GoBackstage from './sections/GoBackstage'
-import Hero from './sections/Hero'
-import Hero2 from './sections/Hero2'
-import LatestNews from './sections/LatestNews'
-import PerformanceSpotlights from './sections/PerformanceSpotlights'
-import PopularEvents from './sections/PopularEvents'
+import '@/styles/global.css'
+import AddEvents from '@/sections/AddEvents'
+import ArtOrganizations from '@/sections/ArtOrganization'
+import BrandFeatured from '@/sections/BrandFeature'
+import CityGuides from '@/sections/CityGuides'
+import Footer from '@/sections/Footer'
+import GoBackstage from '@/sections/GoBackstage'
+import Hero from '@/sections/Hero'
+import Hero2 from '@/sections/Hero2'
+import LatestNews from '@/sections/LatestNews'
+import PerformanceSpotlights from '@/sections/PerformanceSpotlights'
+import PopularEvents from '@/sections/PopularEvents'
+import Navbar from '@/subcomponents/Navbar'
 
 function App () {
   return (
     <div>
+      <Navbar></Navbar>
       <Hero></Hero>
       <BrandFeatured></BrandFeatured>
       <CityGuides></CityGuides>
