dd72215207b77225a66005635a2d4dbfdb3e2338
Charles Nathaniel Togle <149758871+charles-togle@users.noreply.github.com>
2025-05-26 21:26:59 +0800

    Revert "incomplete popular events (seel all and buttons not yet exisitng) (#6)" (#7)
    This reverts commit b19e364383952f959b05054774cd47677d41d6af.

--- src/subcomponents/popular-events-art-organizations/Tag.jsx
+++
@@ -1,7 +0,0 @@
-export default function Tag ({ tag }) {
-  return (
-    <div className='border-1 border-primary-red px-4 py-2 rounded-3xl text-text-1 font-[600] font-content bg-[hsla(358,_72%,_44%,_0.16)]'>
-      {tag}
-    </div>
-  )
-}
