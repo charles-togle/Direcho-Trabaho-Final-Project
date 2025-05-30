a30e7e63b7709d21937ceb14abbe3b95b6f22008
@FerrerMarkAnthony <markferrer0910@gmail.com>
2025-05-30 19:42:47 +0800

    feat: creat branch for performance spotlight

--- src/sections/PerformanceSpotlights.jsx
+++ src/sections/PerformanceSpotlights.jsx
@@ -8,7 +8,7 @@ export default function PerformanceSpotlights() {
           <h1 className="text-white mb-10 text-heading-2 font-bold">
             Performance Spotlights
           </h1>
-          <div className="grid grid-cols-4 gap-7">
+          <div className="grid grid-cols-4 gap-6">
             <div className="h-[390px] bg-gray-300"></div>
             <div className="h-[390px] bg-gray-300"></div>
             <div className="h-[390px] bg-gray-300"></div>
