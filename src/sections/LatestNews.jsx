09f09777365970719e5343a842b2eb4f5cf6dc90
rannedcreyes <scholar.roseannereyes@gmail.com>
2025-06-01 10:57:44 +0800

    container

--- src/sections/LatestNews.jsx
+++ src/sections/LatestNews.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 
+import Container from "@/subcomponents/Container.jsx";
+
 import LatestNewsImage1 from "@/assets/images/latest-news/latest_news_1.png";
 import LatestNewsImage2 from "@/assets/images/latest-news/latest_news_2.png";
 
@@ -21,33 +23,39 @@ function NewsCard({ image, title, description, date }) {
 
 export default function LatestNews() {
   return (
-    <section className="bg-black py-5 text-white">
-      <div className="flex justify-between items-center mb-4">
-        <h2 className="text-lg font-semibold">Latest news</h2>
-        <div className="flex items-center gap-2">
-          <button className="text-white px-4 hover:underline">See All</button>
-          <button className="bg-gray-800 text-white px-3 py-2 rounded-full hover:bg-gray-700">
-            {"<"}
-          </button>
-          <button className="bg-gray-800 text-white px-3 py-2 rounded-full hover:bg-gray-700">
-            {">"}
-          </button>
-        </div>
-      </div>
-      <div className="flex justify-between">
-        <NewsCard
-          image={LatestNewsImage1}
-          title="An exclusive interview with David Brown - Mr. Piano man"
-          description="1-on-1 interview with the legendary band and an inside look at their journey through the years. A read you won’t want to miss!"
-          date="04/18/2021"
-        />
-        <NewsCard
-          image={LatestNewsImage2}
-          title="An exclusive interview with David Brown - Mr. Piano man"
-          description="1-on-1 interview with the legendary band and an inside look at their journey through the years. A read you won’t want to miss!"
-          date="04/18/2021"
-        />
-      </div>
-    </section>
+    <>
+      <Container>
+        <section className="bg-black py-5 text-white">
+          <div className="flex justify-between items-center mb-4">
+            <h2 className="text-lg font-semibold">Latest news</h2>
+            <div className="flex items-center gap-2">
+              <button className="text-white px-4 hover:underline">
+                See All
+              </button>
+              <button className="bg-gray-800 text-white px-3 py-2 rounded-full hover:bg-gray-700">
+                {"<"}
+              </button>
+              <button className="bg-gray-800 text-white px-3 py-2 rounded-full hover:bg-gray-700">
+                {">"}
+              </button>
+            </div>
+          </div>
+          <div className="flex justify-between gap-5">
+            <NewsCard
+              image={LatestNewsImage1}
+              title="An exclusive interview with David Brown - Mr. Piano man"
+              description="1-on-1 interview with the legendary band and an inside look at their journey through the years. A read you won’t want to miss!"
+              date="04/18/2021"
+            />
+            <NewsCard
+              image={LatestNewsImage2}
+              title="An exclusive interview with David Brown - Mr. Piano man"
+              description="1-on-1 interview with the legendary band and an inside look at their journey through the years. A read you won’t want to miss!"
+              date="04/18/2021"
+            />
+          </div>
+        </section>
+      </Container>
+    </>
   );
 }
