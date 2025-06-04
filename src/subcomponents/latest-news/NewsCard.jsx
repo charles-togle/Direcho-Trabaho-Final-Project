8b4b920a43d4d57d9c169c085e0c45de60f66c40
rannedcreyes <scholar.roseannereyes@gmail.com>
2025-06-04 22:35:17 +0800

    refactor: extracted newscard component and mapped latestnews

---
+++ src/subcomponents/latest-news/NewsCard.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const NewsCard = ({ image, title, description, date }) => {
+  return (
+    <div className="flex bg-[#1E1F22] text-white rounded-[16px] w-3/4 h-[282px]">
+      <img src={image} alt={title} className="w-[251px] h-[251px] m-3.5" />
+      <div className="flex-col p-3">
+        <div>
+          <h3 className="text-lg font-semibold mb-8">{title}</h3>
+          <p className="text-sm text-gray-300 mb-17">{description}</p>
+        </div>
+        <p className="text-xs text-gray-400">Published:</p>
+        <p className="text-xs text-gray-400">{date}</p>
+      </div>
+    </div>
+  );
+};
+
+export default NewsCard;
