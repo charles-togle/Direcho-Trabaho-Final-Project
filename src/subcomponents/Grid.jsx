9afb8b9eb757919e9db758ab168042de0bee2364
Mark Anthony Ferrer <105872558+FerrerMarkAnthony@users.noreply.github.com>
2025-06-04 18:45:39 +0800

    Ferrer/refactor spotlight (#18)
    * feat: creating branch for win ticket banner
    
    * feat: Adding hero2/banner section
    
    * feat: Add loading spinner and refactor the spotlight
    
    * fix: mapping in card

---
+++ src/subcomponents/Grid.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+
+const Grid = ({ children, className = "" }) => {
+  return <div className={`grid ${className}`}>{children}</div>;
+};
+export default Grid;
---
+++ src/subcomponents/performance-spotlight/SpotlightCardContent.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Vector from "../../assets/images/performance-spotlights/Vector.png";
+
+const SpotlightCardContent = ({ data }) => {
+  return (
+    <div
+      style={{ backgroundImage: `url(${data.image})` }}
+      className="spotlight__content__container"
+    >
+      <div className="spotlight__content">
+        <div className="spotlight__content__image">
+          <img src={Vector} alt="Vector" />
+        </div>
+      </div>
+      <h2>{data.title}</h2>
+    </div>
+  );
+};
+
+export default SpotlightCardContent;
