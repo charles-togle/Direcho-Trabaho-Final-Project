b964786870bc76fc65ffa9a24e5796d6e31a0eee
charles-togle <149758871+charles-togle@users.noreply.github.com>
2025-06-06 14:24:06 +0800

    changed cursors, changed text size in latest news, fixed ArtOrganizations no name

--- src/subcomponents/SeeAllNavigation.jsx
+++ src/subcomponents/SeeAllNavigation.jsx
@@ -1,32 +1,32 @@
-import React from "react";
+import React from 'react'
 
-import left_icon from "@/assets/icons/left_icon.png";
-import right_icon from "@/assets/icons/right_icon.png";
+import left_icon from '@/assets/icons/left_icon.png'
+import right_icon from '@/assets/icons/right_icon.png'
 
 const SeeAllNavigation = ({ onLeftClick, onRightClick, showSeeAll = true }) => {
   return (
-    <div className="flex items-center gap-2">
+    <div className='flex items-center gap-2'>
       {showSeeAll && (
-        <button className="text-white px-4 hover:underline font-bold">
+        <button className='text-white px-4 hover:underline font-bold cursor-pointer'>
           See All
         </button>
       )}
 
       <button
         onClick={onLeftClick}
-        className="bg-base-shade rounded-full hover:bg-gray-700 w-10 h-10 flex items-center justify-center"
+        className='bg-base-shade rounded-full hover:bg-gray-700 w-10 h-10 flex items-center justify-center cursor-pointer'
       >
-        <img src={left_icon} alt="left arrow" className="w-16px h-7.47px" />
+        <img src={left_icon} alt='left arrow' className='w-16px h-7.47px' />
       </button>
 
       <button
         onClick={onRightClick}
-        className="bg-base-shade rounded-full hover:bg-gray-700 w-10 h-10 flex items-center justify-center"
+        className='bg-base-shade rounded-full hover:bg-gray-700 w-10 h-10 flex items-center justify-center cursor-pointer'
       >
-        <img src={right_icon} alt="right arrow" className="w-16px h-7.47px" />
+        <img src={right_icon} alt='right arrow' className='w-16px h-7.47px' />
       </button>
     </div>
-  );
-};
+  )
+}
 
-export default React.memo(SeeAllNavigation);
+export default React.memo(SeeAllNavigation)
